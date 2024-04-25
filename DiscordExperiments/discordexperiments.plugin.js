080d4ddcc5f4752e28aa05741b555ea3eba79d5b
æm <45801669+Mid0aria@users.noreply.github.com>
2024-04-25 15:19:59 +0300

    Update discordexperiments.plugin.js
    load func

--- DiscordExperiments/discordexperiments.plugin.js
+++ DiscordExperiments/discordexperiments.plugin.js
@@ -20,7 +20,41 @@ var discordExperiments = ((t) =>
             return "0.0.1";
         }
 
-        load() {}
+        load = () => {
+        let _,
+                a = Object.values,
+                b = "getCurrentUser",
+                c = "actionHandler",
+                d = "_actionHandlers",
+                l = "_dispatcher",
+                i = "ExperimentStore";
+            let u = ""; // Define u here
+            let m = "";
+            webpackChunkdiscord_app.push([
+                [Date.now()],
+                {},
+                (e) => {
+                    _ = e;
+                },
+            ]),
+                (m = a(
+                    (u = a(_.c).find(
+                        (e) =>
+                            e?.exports?.default?.[b] &&
+                            e?.exports?.default?.[l]?.[d]
+                    ).exports.default)[l][d]._dependencyGraph.nodes
+                )),
+                (u[b]().flags |= 1),
+                m.find((e) => "Developer" + i == e.name)[c].CONNECTION_OPEN();
+            try {
+                m.find((e) => i == e.name)[c].OVERLAY_INITIALIZE({
+                    user: {
+                        flags: 1,
+                    },
+                });
+            } catch {}
+            m.find((e) => i == e.name).storeDidChange();
+        }
 
         start = () => {
             let _,
